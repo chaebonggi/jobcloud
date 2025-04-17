b9dc0f90d0652d2f312f2ad25086649238d42dee
bonggi <chaebonggi@naver.com>
2025-04-17 11:39:05 +0900

    .

--- jobcloud-root/jobcloud_admin/js/jobcloud/component/run.js
+++ jobcloud-root/jobcloud_admin/js/jobcloud/component/run.js
@@ -232,20 +232,3 @@ $(document).ready(function () {
     
    
 });
-// function includehtml() {
-//     var allElements = $(".include_wrap");
-//     Array.prototype.forEach.call(allElements, function (el) {
-//       var includePath = el.dataset.includePath;
-//       console.log(includePath);
-//       if (includePath) {
-//         var xhttp = new XMLHttpRequest();
-//         xhttp.onreadystatechange = function () {
-//           if (this.readyState == 4 && this.status == 200) {
-//             el.outerHTML = this.responseText;
-//           }
-//         };
-//         xhttp.open('GET', includePath, false);
-//         xhttp.send();
-//       }
-//     });
-//   }
